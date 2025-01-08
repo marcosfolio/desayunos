57a531c40a38e9375c4713c98ea63da1e2ccb2be
Marcos Muñiz <esechinako@gmail.com>
2025-01-08 16:48:48 +0100

    Include nutritional facts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,26 +1,68 @@
 import panDextrinado from '../assets/images/pan_dextrinado.jpg';
 import cremaCacahuete from '../assets/images/crema_cacahuete.jpg';
 
+interface NutritionInfo {
+    energia: number;
+    grasas: number;
+    grasasSaturadas: number;
+    grasasInsaturadas: number;
+    hidratosCarbono: number;
+    azucares: number;
+    proteinas: number;
+}
+
 interface Product {
     name: string;
     image: string;
     link: string;
+    type: string;
+    nutrition: NutritionInfo;
 }
 
 export const products: Product[] = [
     {
         name: "Pan dextrinado",
         image: panDextrinado,
-        link: "https://www.amazon.es/Dextrin-Semillas-Lino-Santiveri-300/dp/B00DN9PEVA"
+        type: "carbohydrate",
+        link: "https://www.amazon.es/Dextrin-Semillas-Lino-Santiveri-300/dp/B00DN9PEVA",
+        nutrition: {
+            energia: 1,
+            grasas: 1,
+            grasasSaturadas: 1,
+            grasasInsaturadas: 1,
+            hidratosCarbono: 1,
+            azucares: 1,
+            proteinas: 1
+        }
     },
     {
         name: "Pan dextrinado",
         image: panDextrinado,
-        link: "https://www.amazon.es/Dextrin-Semillas-Lino-Santiveri-300/dp/B00DN9PEVA"
+        type: "carbohydrate",
+        link: "https://www.amazon.es/Dextrin-Semillas-Lino-Santiveri-300/dp/B00DN9PEVA",
+        nutrition: {
+            energia: 1,
+            grasas: 1,
+            grasasSaturadas: 1,
+            grasasInsaturadas: 1,
+            hidratosCarbono: 1,
+            azucares: 1,
+            proteinas: 1
+        }
     },
     {
         name: "Crema cacahuete",
         image: cremaCacahuete,
-        link: "https://www.amazon.es/Crema-cacahuete-Capit%C3%A1n-cacahuetes-tostados/dp/B08MW9VNH7"
+        type: "protein",
+        link: "https://www.amazon.es/Crema-cacahuete-Capit%C3%A1n-cacahuetes-tostados/dp/B08MW9VNH7",
+        nutrition: {
+            energia: 1,
+            grasas: 1,
+            grasasSaturadas: 1,
+            grasasInsaturadas: 1,
+            hidratosCarbono: 1,
+            azucares: 1,
+            proteinas: 1
+        }
     }
 ]; 
--- src/views/Shopping/Shopping.tsx
+++ src/views/Shopping/Shopping.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
+import {
+    faExternalLink,
+    faDrumstickBite,
+    faBreadSlice
+} from '@fortawesome/free-solid-svg-icons';
 import LinkButton from '../../components/LinkButton/LinkButton';
 import './Shopping.css';
 import { products } from '../../data/products';
@@ -12,8 +16,52 @@ const Shopping = () => {
             <div className="product-grid">
                 {products.map((product, index) => (
                     <div key={index} className="product-card">
+                        <div className="product-tag" data-type={product.type}>
+                            <FontAwesomeIcon
+                                icon={product.type === 'protein' ? faDrumstickBite : faBreadSlice}
+                                className="product-tag-icon"
+                            />
+                            <span>{product.type}</span>
+                        </div>
                         <img src={product.image} alt={product.name} />
                         <h3>{product.name}</h3>
+                        {product.nutrition && (
+                            <div className="nutrition-table">
+                                <h4>Tabla nutricional por 100g</h4>
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>Energía</td>
+                                            <td>{product.nutrition.energia}kcal</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Grasas</td>
+                                            <td>{product.nutrition.grasas}g</td>
+                                        </tr>
+                                        <tr className="warning">
+                                            <td>Grasas saturadas</td>
+                                            <td>{product.nutrition.grasasSaturadas}g</td>
+                                        </tr>
+                                        <tr className="success">
+                                            <td>Grasas insaturadas</td>
+                                            <td>{product.nutrition.grasasInsaturadas}g</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Hidratos de carbono</td>
+                                            <td>{product.nutrition.hidratosCarbono}g</td>
+                                        </tr>
+                                        <tr className="warning">
+                                            <td>Azúcares</td>
+                                            <td>{product.nutrition.azucares}g</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Proteínas</td>
+                                            <td>{product.nutrition.proteinas}g</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        )}
                         <LinkButton
                             href={product.link}
                             text="Buy on Amazon"
